e701621c62302811db1c0fe80e70421288f9f513
kl63 <kl63@NJIT.edu>
2025-02-09 17:40:59 -0500

    first commit

---
+++ IS322_CalculatorApp/IS322_CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IS322_CalculatorAppApp.swift
+//  IS322_CalculatorApp
+//
+//  Created by Kevin Lin on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct IS322_CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
